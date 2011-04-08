6506b0d9632c8f5262e9d7f46631b51bac3fae22
Tony Kemp <tony@agrav.net>
2011-04-08 21:30:53 +1000

    Initial commit of base Rails 3.0.6 app
    Replaces prototype.js with jQuery + jQuery UI.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoap::Application.config.secret_token = '1f0f3e3c793331a8c993e38043a4c800d33f982ebcb963400e677217dee9944069b64df86b845da82984bd400aba5ded2e5ff1ac2bc9f8859182ebee461d6b29'
