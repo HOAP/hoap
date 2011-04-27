ac4ecd482f5021637784aa3ee20ddd30a9c43300
Tony Kemp <tony@agrav.net>
2011-04-27 22:21:32 +1000

    Added participant code validity check.

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,6 +3,17 @@ class SurveyController < ApplicationController
   end
 
   def start
+    if !params[:code].nil? && params[:code] =~ /^[0-9a-f]{8}$/i
+      @participant = Participant.where(:key => params[:code].downcase).first
+      unless @participant.nil? || @participant.completed
+        redirect_to page_url(@participant.key)
+        return
+      end
+      flash[:error] = "Non existent code."
+    else
+      flash[:error] = "Invalid invitation code."
+    end
+    redirect_to :action => 'index'
   end
 
   def page
