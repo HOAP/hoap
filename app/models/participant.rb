010a297938897f3e34175a467920f8a03dc2dd77
Tony Kemp <tony@agrav.net>
2011-05-21 15:34:40 +1000

    Changed AUDIT questionnaire to store answers not scores
    - Actual answers needed for later calculations (page 9)
    - Some questions have multiple answers per score
      value - not helpful for the calculations (noor the
      experimenters)

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,23 @@ class Participant < ActiveRecord::Base
   has_many :answers
   serialize :c_money, Array
 
+  @@audit_values = {
+    5 => {"Never or almost never" => 0, "Less than once a month" => 1, "Once a month" => 1, "Once every two weeks" => 2, "Once a week" => 2, "Two or three times a week" => 3, "Four or five times a week" => 4, "Six or seven times a week" => 4},
+    6 => {"1" => 0, "2" => 0, "3" => 1, "4" => 1, "5" => 2, "6" => 2, "7" => 3, "8" => 3, "9" => 3, "10" => 4, "11" => 4, "12" => 4, "13" => 4, "14" => 4, "15" => 4, "16" => 4, "17" => 4, "18" => 4, "19" => 4, "20" => 4, "21" => 4, "22" => 4, "23" => 4, "24" => 4, "25-29" => 4, "30-34" => 4, "35-39" => 4, "40-49" => 4, "50 or more" => 4},
+    7 => {"Never" => 0, "Once or twice a year" => 1, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    8 => {"Never" => 0, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    9 => {"Never" => 0, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    10 => {"Never" => 0, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    11 => {"Never" => 0, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    12 => {"Never" => 0, "Less than monthly" => 1, "Monthly" => 2, "Weekly" => 3, "Daily or almost daily" => 4},
+    13 => {"No" => 0, "Yes, but not in the last year" => 2, "Yes, during the last year" => 4},
+    14 => {"No" => 0, "Yes, but not in the last year" => 2, "Yes, during the last year" => 4}
+  }
+
+  def audit_score(question, answer)
+    @@audit_values[question][answer]
+  end
+
   def self.make
     begin
       key = Digest::SHA1.hexdigest("#{rand} - #{Time.now.to_f}")[0..11]
