71c29ef3280842f09bc4370cc0486dda078e0317
Tony Kemp <tony@agrav.net>
2011-05-21 16:12:36 +1000

    Added methods Participant to calculate calculated fields

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -52,4 +52,75 @@ class Participant < ActiveRecord::Base
     end
     self.save
   end
+
+  # The AUDIT score (page 4)
+  def audit
+    if self.c_audit.nil?
+      answers = Answer.where(:participant_id => self.id, :page => 4).order("id ASC")
+      self.c_audit = answers.reduce(0) do |sum, a|
+        sum + audit_score(a.question_id, a.value)
+      end
+      self.save
+    end
+    self.c_audit
+  end
+
+  # The Leeds Dependence Questionnaire score (page 7)
+  def ldq
+    if self.c_ldq.nil?
+      answers = Answer.where(:participant_id => self.id, :page => 7).order("id ASC")
+      self.c_ldq = answers.reduce(0) do |sum, a|
+        sum + a.value.to_i
+      end
+      self.save
+    end
+    self.c_ldq
+  end
+
+  def typical_drinks
+    Answer.where(:participant_id => self.id, :question_id => 6).select(:value).first.value
+  end
+
+  def dpw
+    if self.c_dpw.nil?
+      drinks = typical_drinks.to_i
+      frequency = Answer.where(:participant_id => self.id, :question_id => 5).select(:value).first.value
+      case frequency
+      when /^Never/
+        mult = 0
+      when /^(Less)|(Once a month)/
+        mult = 0.25
+      when /two weeks$/
+        mult = 0.5
+      when /Once a week/
+        mult = 1
+      when /^Two/
+        mult = 2.5
+      when /^Four/
+        mult = 4.5
+      when /^Six/
+        mult = 6.5
+      end
+      self.c_dpw = drinks * mult
+    end
+    self.c_dpw
+  end
+
+  def money
+    if self.c_money.nil?
+      dpy = self.dpw * 52
+      # TODO: get correct minimum and maximum dollar value per drink.
+      self.c_money = [dpy * 1.5, dpy * 6.5]
+      self.save
+    end
+    self.c_money
+  end
+
+  def bac
+    if self.c_bac.nil?
+      # TODO: find correct method of calculating BAC.
+      self.c_bac = 0.13
+    end
+    self.c_bac
+  end
 end
