661cacf7fb36a5c3be805286e23b6b13b2d7981f
Tony Kemp <tony@agrav.net>
2012-03-09 07:42:08 +1100

    Fixed config for asset recompilation on Heroku.

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module Hoap
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Do not initialise the whole app to compile assets - this fails on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
