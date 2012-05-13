7fcb98155026eecdeaf19c76608f3da0d7582aeb
Tony Kemp <tony@agrav.net>
2012-05-13 11:50:47 +1000

    Add completed count and available codes to index.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,8 @@ class AdminController < ApplicationController
   before_filter :require_admin_user
 
   def index
+    @count = Participant.where(:completed => true).count
+    @available = Participant.where(:page => 1).pluck(:key)
   end
 
   def participant
