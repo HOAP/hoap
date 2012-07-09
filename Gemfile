511464442bc3a45949eeb912d0f7e4ad2ca874b5
Tony Kemp <tony@agrav.net>
2012-07-09 21:31:25 +1000

    Adds PDF rendering of report page.
    Use PDFKit as Rack middleware to render report page as PDF file.
    Add print media CSS to fix layout for PDF.
    Refactor CSS files - move all report stuff to report.css.
    Move CSS files around a bit so asset pipeline can correctly handle
    print CSS.

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'haml'
 gem 'thin'
 gem 'rmagick', :require => 'RMagick'
 gem 'gruff'
+gem 'pdfkit'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'pdfkit'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -58,5 +59,7 @@ module Hoap
 
     # Do not initialise the whole app to compile assets - this fails on Heroku
     config.assets.initialize_on_precompile = false
+
+    config.middleware.use PDFKit::Middleware, {:page_size => :A4}, :only => %r[^/report]
   end
 end
