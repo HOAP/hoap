826e70d40640b15228d5b7a9b1fac16cf4b0a589
Tony Kemp <tony.kemp@newcastle.edu.au>
2012-08-17 14:34:38 +1000

    Create basic form.

--- Decryptor/DecryptorForm.Designer.cs
+++ Decryptor/DecryptorForm.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.gbDataFile = new System.Windows.Forms.GroupBox();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.dataFileBox = new System.Windows.Forms.TextBox();
+            this.gbDataFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(199, 74);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 0;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            // 
+            // gbDataFile
+            // 
+            this.gbDataFile.Controls.Add(this.dataFileBox);
+            this.gbDataFile.Controls.Add(this.fileButton);
+            this.gbDataFile.Location = new System.Drawing.Point(12, 12);
+            this.gbDataFile.Name = "gbDataFile";
+            this.gbDataFile.Size = new System.Drawing.Size(268, 56);
+            this.gbDataFile.TabIndex = 1;
+            this.gbDataFile.TabStop = false;
+            this.gbDataFile.Text = "Data File";
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(187, 18);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(75, 23);
+            this.fileButton.TabIndex = 0;
+            this.fileButton.Text = "File...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            // 
+            // dataFileBox
+            // 
+            this.dataFileBox.Location = new System.Drawing.Point(7, 20);
+            this.dataFileBox.Name = "dataFileBox";
+            this.dataFileBox.Size = new System.Drawing.Size(174, 20);
+            this.dataFileBox.TabIndex = 1;
+            // 
+            // DecryptorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 104);
+            this.Controls.Add(this.gbDataFile);
+            this.Controls.Add(this.decryptButton);
+            this.Name = "DecryptorForm";
+            this.Text = "HOAP Decryptor";
+            this.gbDataFile.ResumeLayout(false);
+            this.gbDataFile.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.GroupBox gbDataFile;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.TextBox dataFileBox;
     }
 }
 
