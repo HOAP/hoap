9553e9c8e0aef3719faf9b25e319b9b12839a0d5
Tony Kemp <tony@agrav.net>
2012-08-21 09:39:20 +1000

    Replaces .net crypto with Bouncy Castle.
    The Bouncy Castle crypto library can read PEM format certificates,
    whereas the .net libraries need an XML format that I am having great
    trouble in convereting the private key to from PEM format.
    
    http://www.bouncycastle.org/csharp/

--- Decryptor/Decryptor.cs
+++ Decryptor/Decryptor.cs
@@ -2,24 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Security.Cryptography;
 using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.OpenSsl;
 
 namespace Decryptor
 {
+    /// <summary>
+    /// This class does the actual decryption.
+    /// I am using the Bouncy Castle crypto library (http://www.bouncycastle.org/csharp/)
+    /// because it can read PEM format private certificates, unlike the .net system
+    /// library.
+    /// </summary>
     public class Decryptor
     {
-        private RSACryptoServiceProvider rsa;
+        private AsymmetricCipherKeyPair keyPair;
+        private Pkcs1Encoding decryptEngine;
 
         public Decryptor()
         {
-            rsa = new RSACryptoServiceProvider();
-            rsa.FromXmlString(File.ReadAllText(@"private.xml"));
+
+            using (var reader = File.OpenText(@"private.pem"))
+            {
+                keyPair = (AsymmetricCipherKeyPair)new PemReader(reader).ReadObject();
+            }
+            decryptEngine = new Pkcs1Encoding(new RsaEngine());
+            decryptEngine.Init(false, keyPair.Private);
         }
 
         public string DecryptString(string encText)
         {
-            byte[] plain = rsa.Decrypt(Convert.FromBase64String(encText), false);
+            byte[] cypher = Convert.FromBase64String(encText);
+            byte[] plain = decryptEngine.ProcessBlock(cypher, 0, cypher.Length);
             return Encoding.UTF8.GetString(plain);
         }
     }
